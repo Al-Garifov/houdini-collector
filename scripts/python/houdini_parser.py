3a78dd5289ce09a7c64341e5b88ba206e59ae404
Alexey Garifov <al.garifov@gmail.com>
2024-04-21 17:28:19 +0300

    progress bar added

--- scripts/python/houdini_parser.py
+++ scripts/python/houdini_parser.py
@@ -8,24 +8,27 @@ import hou
 
 
 def run():
-    start_frame = int(hou.playbar.frameRange()[0])
-    end_frame = int(hou.playbar.frameRange()[1])
-    importlib.reload(hip_parser)
-    importlib.reload(usd_parser)
-    refs = set()
-    for frame in range(start_frame, end_frame + 1, 1):
-        hip_refs = hip_parser.parse(frame=frame)
-        for ref in hip_refs:
-            if ref in refs:
-                continue
-            if ".usd" in ref:
-                refs.update(usd_parser.get_asset_paths(ref))
-                refs.update(usd_parser.get_all_references(ref))
-        refs.update(hip_refs)
+    with hou.InterruptableOperation("Parsing scene...", open_interrupt_dialog=True) as oper:
+        oper.updateProgress(0)
+        start_frame = int(hou.playbar.frameRange()[0])
+        end_frame = int(hou.playbar.frameRange()[1])
+        importlib.reload(hip_parser)
+        importlib.reload(usd_parser)
+        refs = set()
+        for frame in range(start_frame, end_frame + 1, 1):
+            oper.updateProgress(frame/end_frame)
+            hip_refs = hip_parser.parse(frame=frame)
+            for ref in hip_refs:
+                if ref in refs:
+                    continue
+                if ".usd" in ref:
+                    refs.update(usd_parser.get_asset_paths(ref))
+                    refs.update(usd_parser.get_all_references(ref))
+            refs.update(hip_refs)
 
-    # FIXME: fix case-sensitive scenarios
-    fixed_refs = set()
-    for ref in refs:
-        fixed_refs.add(ref.lower())
+        # FIXME: fix case-sensitive scenarios
+        fixed_refs = set()
+        for ref in refs:
+            fixed_refs.add(ref.lower())
 
     return fixed_refs
